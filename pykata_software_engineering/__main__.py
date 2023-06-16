7f6dcdcc78c501c40e4530695973a7968cb3d8bb
doug-cresswell <doug-cresswell@users.noreply.github.com>
2023-06-16 09:07:51 +0000

    ✅ Ready to clone and code.

--- pykata_software_engineering/__main__.py
+++ pykata_software_engineering/__main__.py
@@ -1,4 +1,4 @@
-"""Entry point for project_name."""
+"""Entry point for pykata_software_engineering."""
 
 from .cli import main  # pragma: no cover
 
--- pykata_software_engineering/cli.py
+++ pykata_software_engineering/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for pykata_software_engineering project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m pykata_software_engineering` and `$ pykata_software_engineering `.
 
     This is your program's entry point.
 
